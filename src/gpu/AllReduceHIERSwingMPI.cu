cb424f9f16fad1eeee69faaac00980c53e4a80d1
Daniele De Sensi <d.desensi.software@gmail.com>
2025-04-06 02:36:33 +0200

    working

--- src/gpu/AllReduceHIERSwingMPI.cu
+++ src/gpu/AllReduceHIERSwingMPI.cu
@@ -1165,7 +1165,7 @@ int main(int argc, char *argv[]) {
     CUDA_CHECK(cudaMalloc((void**)&d_test_recv_buffer, (size_t) BUFFER_SIZE));
       
     for (int i = 0; i < msg_count; i++) {
-        h_send_buffer[i] = rand()*rank % 100; 
+        h_send_buffer[i] = rand()*rank % 2; 
     }
 
     // Create the inter and intra communicator

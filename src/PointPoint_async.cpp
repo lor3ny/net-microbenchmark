d97ef29e6633206643f9b82447e6e1e439b33c8d
Lorenzo Piarulli <54499060+lor3ny@users.noreply.github.com>
2025-07-19 15:56:19 +0200

    Point2Point Async (maybe I should not use vectors, lets see)

--- src/PointPoint_async.cpp
+++ src/PointPoint_async.cpp
@@ -2,18 +2,30 @@
 
 using namespace std;
 
+#define B1 1
+#define KiB1 1024
+#define MiB1 1048576
+#define GiB1 1073741824
+
+
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
 
     int rank, size, name_len, ret;
+    double total_time = 0.0;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Get_processor_name(processor_name, &name_len);
 
+    if(size != 2){
+      cerr << "Point-Point requires only 2 procs!" << endl;
+      return 1;
+    }
+
     if (argc < 3) {
-        cerr << "Please, insert an integer as argument" << endl;
-        return 1;
+      cerr << "Please, insert an integer as argument" << endl;
+      return 1;
     }
 
     int size_count = 0;
@@ -59,36 +71,53 @@ int main(int argc, char *argv[]) {
   
 
     int BUFFER_SIZE = (size_count * multiplier_type);
-    unsigned char *send_buffer = (unsigned char*) malloc_align(BUFFER_SIZE*size); 
-    unsigned char *recv_buffer = (unsigned char*) malloc_align(BUFFER_SIZE*size);
+    int msg_count = BUFFER_SIZE/sizeof(int);
+    int *send_buffer = (int*) malloc_align(BUFFER_SIZE*size);
+    int *recv_buffer = (int*) malloc_align(BUFFER_SIZE*size);
     if (send_buffer == NULL || recv_buffer == NULL) {
         fprintf(stderr, "Memory allocation failed!\n");
         MPI_Abort(MPI_COMM_WORLD, 1);
         return -1;
     }
 
-    for (int i = 0; i < BUFFER_SIZE*size; i++) {
-        send_buffer[i] = 'a'; 
+    for (int i = 0; i < msg_count; i++) {
+        send_buffer[i] = (int) (rand()*rank % 10);
     }
 
     double* samples = (double*) malloc_align(sizeof(double) * BENCHMARK_ITERATIONS);
-    double* samples_all = (double*) malloc_align(sizeof(double) * BENCHMARK_ITERATIONS * size);
+    double* samples_all = (double*) malloc_align(sizeof(double) * BENCHMARK_ITERATIONS);
+    std::vector<MPI_Request> requests;
     MPI_Barrier(MPI_COMM_WORLD);
     for(int i = 0; i < BENCHMARK_ITERATIONS + WARM_UP; ++i){
 
         double start_time, end_time;
+        MPI_Request req_recv;
+        MPI_Request req_send;
+
         start_time = MPI_Wtime();
-        MPI_Alltoall(send_buffer, BUFFER_SIZE, MPI_BYTE, recv_buffer, BUFFER_SIZE, MPI_BYTE, MPI_COMM_WORLD);
+        if(rank == 0){
+          MPI_Isend(send_buffer, msg_count, MPI_INT, 1, 0, MPI_COMM_WORLD, req_send);
+          MPI_Irecv(recv_buffer, msg_count, MPI_INT, 1, 0, MPI_COMM_WORLD, req_recv);
+        }else if(rank == 1){
+          MPI_Isend(send_buffer, msg_count, MPI_INT, 0, 0, MPI_COMM_WORLD, req_send);
+          MPI_Irecv(recv_buffer, msg_count, MPI_INT, 0, 0, MPI_COMM_WORLD, req_recv);
+        }
+        requests.push_back(req_send);
+        requests.push_back(req_recv);
+        MPI_Waitall(static_cast<int>(requests.size()), requests.data(), MPI_STATUSES_IGNORE);
         end_time = MPI_Wtime();
 
         if(i>WARM_UP) {
           samples[i-WARM_UP] = (end_time - start_time);
+          total_time += (end_time - start_time);
         }
 
         MPI_Barrier(MPI_COMM_WORLD);
     }
+    total_time = (double)(total_time)/BENCHMARK_ITERATIONS;
 
     double max_time;
+    MPI_Reduce(&total_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
     MPI_Reduce(samples, samples_all, BENCHMARK_ITERATIONS, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
     MPI_Barrier(MPI_COMM_WORLD);

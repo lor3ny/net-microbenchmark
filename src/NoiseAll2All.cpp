e9bc0deaa7c6a0200f1d1d2149bd61d486ada6f3
Lorenzo Piarulli <54499060+lor3ny@users.noreply.github.com>
2025-07-28 11:59:16 +0200

    buffer change and some log

--- src/NoiseAll2All.cpp
+++ src/NoiseAll2All.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    const int small_buf_size = 2 * 1024 * 1024;  // bytes per peer 16KiB
+    const int small_buf_size = 16 * 1024 * 1024;  // bytes per peer 16KiB
 
     // Each process will send a chunk to every other process
     std::vector<char> send_buf(small_buf_size * world_size, world_rank);
@@ -18,6 +18,7 @@ int main(int argc, char** argv) {
         MPI_Alltoall(send_buf.data(), small_buf_size, MPI_CHAR,
                      recv_buf.data(), small_buf_size, MPI_CHAR,
                      MPI_COMM_WORLD);
+        cerr << "Done." << endl;
     }
 
     MPI_Finalize();

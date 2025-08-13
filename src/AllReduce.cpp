264885ebfc2de31c84d4edac2594dcdad51550c9
Lorenzo Piarulli <54499060+lor3ny@users.noreply.github.com>
2025-08-13 10:00:55 +0200

    no mem

--- src/AllReduce.cpp
+++ src/AllReduce.cpp
@@ -318,8 +318,8 @@ int main(int argc, char *argv[]) {
     for(int i = 0; i < BENCHMARK_ITERATIONS + WARM_UP; ++i){
 
         double start_time, end_time;
-        start_time = MPI_Wtime();
         allreduce_memory(send_buffer, recv_buffer, msg_count, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+        start_time = MPI_Wtime();
         allreduce_ring(send_buffer, recv_buffer, msg_count, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
         //MPI_Allreduce(send_buffer, recv_buffer, msg_count, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
         end_time = MPI_Wtime();
